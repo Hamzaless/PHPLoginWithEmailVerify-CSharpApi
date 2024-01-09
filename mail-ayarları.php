29286bdac45dc63a3c4e34b69816deb6f9ff5cf4
Hamza Taşdöven <89343438+Hamzaless@users.noreply.github.com>
2024-01-09 17:46:49 +0300

    Mail Ayarları güncellendi

--- mail-ayarları.php
+++ mail-ayarları.php
@@ -5,9 +5,9 @@ return array(
     // sistemin mail atabilmesi için önemlidir
     // daha detaylı ayar yapmak için 'kayıt-ol.php' dosyasındaki 'sendMail()' fonksiyonuna göz atın
     "base_url" => "http://localhost/login/", // 'index.php' bulunduğu klasör (gerekli)
-    "host" => "smtp.yandex.com", // SMTP sunucusu
-    "username" => "FullSharpDeveloper@yandex.com", // SMTP kullanıcı adı
-    "password" => "smtp şifresi & yandex kullanmanızı öneriyorum!", // SMTP şifre
+    "host" => "smtp.mrhamzaless.com", // SMTP sunucusu
+    "username" => "noreply@mrhamzaless.com", // SMTP kullanıcı adı
+    "password" => "smtp şifresi", // SMTP şifre
     "port" => "465" // SMTP port
 );
 
